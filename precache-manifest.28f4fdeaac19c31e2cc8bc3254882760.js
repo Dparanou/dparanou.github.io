7669dedb374cb5588a3efa11f187db75ffa90559
dparanou <dimitra.pra@gmail.com>
2022-09-19 12:05:21 +0300

    init commit

---
+++ precache-manifest.28f4fdeaac19c31e2cc8bc3254882760.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "098b7cfc132b7a583762",
+    "url": "/static/css/main.d3afe8b5.chunk.css"
+  },
+  {
+    "revision": "098b7cfc132b7a583762",
+    "url": "/static/js/main.098b7cfc.chunk.js"
+  },
+  {
+    "revision": "7a2ee09bee8492585ee7",
+    "url": "/static/js/1.7a2ee09b.chunk.js"
+  },
+  {
+    "revision": "3eee752de2797da93b0d",
+    "url": "/static/css/2.af2f2944.chunk.css"
+  },
+  {
+    "revision": "3eee752de2797da93b0d",
+    "url": "/static/js/2.3eee752d.chunk.js"
+  },
+  {
+    "revision": "c383990e76a5d292c829",
+    "url": "/static/js/runtime~main.c383990e.js"
+  },
+  {
+    "revision": "39475528dae655ea7c0d44f618708a08",
+    "url": "/static/media/20220915_3.39475528.jpg"
+  },
+  {
+    "revision": "672d982a0eb19e2941b021dc65373e41",
+    "url": "/static/media/Energy_saving.672d982a.png"
+  },
+  {
+    "revision": "1b71e82e5a94f28bef8da984933d1909",
+    "url": "/static/media/athenaLogo.1b71e82e.jpeg"
+  },
+  {
+    "revision": "1235be1be9a182e9b890a1d72b35313c",
+    "url": "/static/media/green_city.1235be1b.jpeg"
+  },
+  {
+    "revision": "bfa8d1d09ba7c716283d4f1e6353dfca",
+    "url": "/static/media/green_city2.bfa8d1d0.jpeg"
+  },
+  {
+    "revision": "f4aaddc34b788a59c07215bd7a421bf9",
+    "url": "/static/media/architecture.f4aaddc3.png"
+  },
+  {
+    "revision": "8f8cd75d77ca8c919fc57950f5169416",
+    "url": "/static/media/logo.8f8cd75d.png"
+  },
+  {
+    "revision": "89183635dafb01230a6f0c1da1ae432c",
+    "url": "/index.html"
+  }
+];
